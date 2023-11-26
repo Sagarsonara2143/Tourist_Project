bc6b574be0770c114d8e9464bb25eb817276ce66
Sagar Sonara <sagarsonara2143@gmail.com>
2023-11-26 12:08:35 +0530

    26.11.2023

--- myapp/views.py
+++ myapp/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-	return render(request,"index.html")
+	return render(request,"index.html",{'is_home_page': True})
+
+
+def about(request):
+	return render(request,"about.html",{'is_about_page': True})
